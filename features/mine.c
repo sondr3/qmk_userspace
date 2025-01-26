c1a4a61464a5686ca69e5417bce497aea00ea31e
Sondre Aasemoen <sondre@eons.io>
2025-01-26 15:25:57 +0100

    Format and rename all keys to align things

--- features/mine.c
+++ features/mine.c
@@ -1,6 +1,32 @@
+// Layers used in my keyboards
 enum layers {
 	BASE,
 	SYM,
 	NUM,
 	SYS
 };
+
+// Too few fingers macros
+#define M_CST LCTL(LSFT(KC_TAB)) // Ctrl + Shift + Tab
+#define M_CT LCTL(KC_TAB) // Ctrl + Tab
+
+// Layer macros
+#define M_SPC LT(SYM, KC_SPACE)
+#define M_BSPC LT(NUM, KC_SPACE)
+
+// Left hand side macros
+#define M_A MT(KC_LGUI, KC_A)
+#define M_R MT(KC_LALT, KC_R)
+#define M_S MT(KC_LCTL, KC_S)
+#define M_T MT(KC_LSFT, KC_T)
+
+// Right hand side macros
+#define M_N MT(KC_RSFT, KC_N)
+#define M_E MT(KC_RCTL, KC_E)
+#define M_I MT(KC_RALT, KC_I)
+#define M_O MT(KC_RGUI, KC_O)
+
+// Norwegian macros
+#define M_AE LALT(KC_QUOTE)
+#define M_OE LALT(KC_O)
+#define M_AA LALT(KC_A)
--- keyboards/zsa/voyager/keymaps/mine/keymap.c
+++ keyboards/zsa/voyager/keymaps/mine/keymap.c
@@ -8,31 +8,31 @@
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_voyager(
-    KC_GRV  , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
-    KC_ESC  , KC_Q  , KC_W  , KC_F  , KC_P  , KC_B  ,                                           KC_J,           KC_L,           KC_U,           KC_Y,           KC_SCLN,        KC_EQUAL,       
-    MT(MOD_LSFT, KC_BSPC),MT(MOD_LGUI, KC_A),MT(MOD_LALT, KC_R),MT(MOD_LCTL, KC_S),MT(MOD_LSFT, KC_T),KC_G,                                           KC_M,           MT(MOD_RSFT, KC_N),MT(MOD_RCTL, KC_E),MT(MOD_RALT, KC_I),MT(MOD_RGUI, KC_O),MT(MOD_RSFT, KC_QUOTE),
-    _______, KC_Z   , KC_X  , KC_C  , KC_D  , KC_V  ,                                           KC_K,           KC_H,           KC_COMMA,       KC_DOT,         KC_SLASH,       MO(3),          
-                                              LT(1,KC_SPACE), KC_TAB,                                         KC_ENTER,       LT(2,KC_BSPC)
+    KC_GRV  , KC_1     , KC_2   , KC_3     , KC_4    , KC_5    ,                      KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_MINUS ,
+    KC_ESC  , KC_Q     , KC_W   , KC_F     , KC_P    , KC_B    ,                      KC_J    , KC_L    , KC_U    , KC_Y    , KC_SCLN , KC_EQUAL ,
+    KC_BSPC , M_A      , M_R    , M_S      , M_T     , KC_G    ,                      KC_M    , M_N     , M_E     , M_I     , M_O     , KC_QUOTE ,
+    _______ , KC_Z     , KC_X   , KC_C     , KC_D    , KC_V    ,                      KC_K    , KC_H    , KC_COMM , KC_DOT  , KC_SLSH , MO(3)    ,
+                                                       M_SPC   , KC_TAB ,    KC_ENT , M_BSPC
   ),
   [SYM] = LAYOUT_voyager(
-    KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                                          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         
-    KC_ASTR,        KC_EXLM,        KC_LCBR,        KC_RCBR,        KC_PIPE,        KC_PERC,                                        KC_7,           KC_8,           KC_9,           KC_MINUS,       KC_SLASH,       KC_F12,         
-    _______, KC_DLR,         KC_LPRN,        KC_RPRN,        KC_GRAVE,       KC_HASH,                                        KC_4,           KC_5,           KC_6,           KC_PLUS,        KC_ASTR,        KC_BSPC,        
-    _______, KC_AMPR,        KC_LBRC,        KC_RBRC,        KC_TILD,        KC_CIRC,                                        KC_1,           KC_2,           KC_3,           KC_DOT,         KC_BSLS,        KC_ENTER,       
-                                                    _______, _______,                                 _______, KC_0
+    KC_ESC  , KC_F1   , KC_F2   , KC_F3   , KC_F4    , KC_F5   ,                      KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  ,
+    KC_ASTR , KC_EXLM , KC_LCBR , KC_RCBR , KC_PIPE  , KC_PERC ,                      KC_7    , KC_8    , KC_9    , KC_MINS , KC_SLSH , KC_F12  ,
+    _______ , KC_DLR  , KC_LPRN , KC_RPRN , KC_GRAVE , KC_HASH ,                      KC_4    , KC_5    , KC_6    , KC_PLUS , KC_ASTR , KC_BSPC ,
+    _______ , KC_AMPR , KC_LBRC , KC_RBRC , KC_TILD  , KC_CIRC ,                      KC_1    , KC_2    , KC_3    , KC_DOT  , KC_BSLS , KC_ENT  ,
+                                                       _______ , _______,    _______, KC_0
   ),
   [NUM] = LAYOUT_voyager(
-    _______, _______, _______, _______, _______, _______,                                 _______, _______, _______, _______, _______, _______, 
-    _______, _______, _______, _______, _______, _______,                                 _______, _______, KC_UP,          _______, _______, _______, 
-    _______, _______, _______, _______, _______, _______,                                 _______, KC_LEFT,        KC_DOWN,        KC_RIGHT,       _______, _______, 
-    _______, _______, LALT(KC_QUOTE), LALT(KC_O),     LALT(KC_A),     _______,                                 _______, _______, _______, _______, _______, _______, 
-                                                    _______, _______,                                 _______, _______
+    _______ , _______ , _______ , _______ , _______ , _______ ,                       _______ , _______ , _______ , _______ , _______ , _______ ,
+    _______ , _______ , _______ , _______ , _______ , _______ ,                       _______ , _______ , KC_UP   , _______ , _______ , _______ ,
+    _______ , _______ , _______ , _______ , _______ , _______ ,                       _______ , KC_LEFT , KC_DOWN , KC_RGHT , _______ , _______ ,
+    _______ , _______ , M_AE    , M_OE    , M_AA    , _______ ,                       _______ , _______ , _______ , _______ , _______ , _______ ,
+                                                      _______ , _______,     _______, _______
   ),
   [SYS] = LAYOUT_voyager(
-    RM_TOGG,        QK_KB,RM_NEXT,RGB_M_P,        UG_VALD,        UG_VALU,                                        _______, _______, _______, _______, _______, QK_BOOT,        
-    _______, _______, KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_AUDIO_MUTE,  _______,                                 KC_PAGE_UP,     KC_HOME,        _______, KC_END,         _______, _______, 
-    _______, KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_MEDIA_STOP,  KC_MEDIA_PLAY_PAUSE,_______,                                 KC_PGDN,        _______, _______, _______, _______, _______, 
-    _______, _______, _______, _______, _______, _______,                                 _______, LCTL(LSFT(KC_TAB)),LCTL(KC_TAB),   _______, _______, _______, 
-                                                    _______, _______,                                 _______, _______
+    RM_TOGG , QK_KB   , RM_NEXT , RGB_M_P , UG_VALD , UG_VALU ,                       _______ , _______ , _______ , _______ , _______ , QK_BOOT ,
+    _______ , _______ , KC_VOLD , KC_VOLU , KC_MUTE , _______ ,                       KC_PGUP , KC_HOME , _______ , KC_END  , _______ , _______ ,
+    _______ , KC_MPRV , KC_MNXT , KC_MSTP , KC_MPLY , _______ ,                       KC_PGDN , _______ , _______ , _______ , _______ , _______ ,
+    _______ , _______ , _______ , _______ , _______ , _______ ,                       _______ , M_CST   , M_CT    , _______ , _______ , _______ ,
+                                                      _______ , _______,     _______, _______
   ),
 };

a67e8e29505537570b6a4d5cd034d461fe21a000
Sondre Aasemoen <sondre@eons.io>
2026-01-08 14:07:23 +0100

    Whoops, copy pasta failure for Voyager layout

--- keyboards/zsa/voyager/keymaps/mine/keymap.c
+++ keyboards/zsa/voyager/keymaps/mine/keymap.c
@@ -43,6 +43,5 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
     '*' , 'L' , 'L' , 'L' , 'L' , 'L' ,         'R' , 'R' , 'R' , 'R' , 'R' , '*' ,
     '*' , 'L' , 'L' , 'L' , 'L' , 'L' ,         'R' , 'R' , 'R' , 'R' , 'R' , '*' ,
     '*' , 'L' , 'L' , 'L' , 'L' , 'L' ,         'R' , 'R' , 'R' , 'R' , 'R' , '*' ,
-    '*' , 'L' , 'L' , 'L' , 'L' , 'L' ,         'R' , 'R' , 'R' , 'R' , 'R' , '*' ,
                             '*' , '*' ,         '*' , '*'
   );
